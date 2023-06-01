625254818f6b6f61a8c1f6ac95412ffc0fb94ab6
emolinac <emolinac92@gmail.com>
2023-06-01 14:32:08 -0400

    upd: new colors

--- run-broadening-analysis.sh
+++ run-broadening-analysis.sh
@@ -30,9 +30,9 @@ cd ./bin
 
 ## Integration of PhiPQ distributions
 #./integrate-phi-q2nuzhpt2
-#
-## Background reduction in Pt2 distributions
-#./bgreduction-pt2-q2nuzh
+
+# Background reduction in Pt2 distributions
+./bgreduction-pt2-q2nuzh
 
 # RC Fix process
 ./rcfactors-fix-fit
@@ -48,8 +48,8 @@ cd ./bin
 ./print-plot-acccorr-uncorr-comparison
 
 # Plots
-#./print-plot-meanpt2-q2nuzh
-#./print-plot-meanpt2-q2-nu-zh-a13
-#./print-plot-broadening-q2nuzh
-#./print-plot-broadening-q2nuzha13
-#./print-plot-broadening-q2-nu-zh-a13
+./print-plot-meanpt2-q2nuzh
+./print-plot-meanpt2-q2-nu-zh-a13
+./print-plot-broadening-q2nuzh
+./print-plot-broadening-q2nuzha13
+./print-plot-broadening-q2-nu-zh-a13
